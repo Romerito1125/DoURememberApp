f90dd062fecb06d0e4fb8181d5ebae0606836545
Romerito1125 <104238155+Romerito1125@users.noreply.github.com>
2025-11-11 22:12:10 -0500

    Actualizar auth.service.ts

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -23,7 +23,7 @@ export interface CreateUserDto {
   status?: string
 }
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://34.117.162.170'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.devcorebits.com'
 
 class AuthService {
   private supabase = createClient()
